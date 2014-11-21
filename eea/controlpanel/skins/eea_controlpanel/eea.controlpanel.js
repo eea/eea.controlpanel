1dee211f2912f0d44c6eb77c84d2650d405ca479
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2014-11-21 19:27:22 +0200

    Created the interface
    Created the draggable and resizable inerface of the panels, refs #21853

---
+++ eea/controlpanel/skins/eea_controlpanel/eea.controlpanel.js
@@ -0,0 +1,10 @@
+$(function() {
+    $( "#panel1" ).draggable();
+    $( "#panel1" ).resizable();
+
+    $( "#panel2" ).draggable();
+    $( "#panel2" ).resizable();
+
+    $( "#panel3" ).draggable();
+    $( "#panel3" ).resizable();
+});
