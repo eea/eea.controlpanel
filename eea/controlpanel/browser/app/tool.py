04a7df52e44e561c367bcdf462dff449e3556ddb
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2014-11-24 19:56:26 +0200

    Implemented database activity panel, refs #21853

--- eea/controlpanel/browser/app/tool.py
+++ eea/controlpanel/browser/app/tool.py
@@ -1,14 +1,52 @@
 """ Controllers
 """
 import logging
+import json
 from zope.component import queryUtility, queryAdapter
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
+from DateTime import DateTime
+from plone import api
 
 
 logger = logging.getLogger('eea.controlpanel')
+
+class PythonObjectEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (list, dict, str, unicode, int, float, bool, type(None))):
+            return json.JSONEncoder.default(self, obj)
+        elif isinstance(obj, set):
+            return list(obj)
+        elif isinstance(obj, DateTime):
+            return obj.asdatetime().strftime("%Y-%m-%d %H:%M:%S")
+        return {'_python_object': pickle.dumps(obj)}
+
+def jsonify(data, response=None, status=None):
+    """ Convert obj to JSON
+    """
+    if response:
+        response.setHeader("Content-type", "application/json")
+        if status:
+            response.setStatus(status)
+    #return json.dumps(data)
+    return PythonObjectEncoder(indent=2, sort_keys=True).encode(data)
+
 
 class ControlPanel(BrowserView):
     """
     """
 
+
+class ControlPanelDBActivity(BrowserView):
+    """
+    """
+
+    def __call__(self, **kwargs):
+        """
+        """
+        portal = api.portal.get()
+        return jsonify({
+            "name": "EEA Control Panel API",
+            "log": portal.undoable_transactions(first_transaction=0,
+                                                last_transaction=10000)
+            }, self.request.response)
--- eea/controlpanel/skins/eea_controlpanel/eea.controlpanel.js
+++ eea/controlpanel/skins/eea_controlpanel/eea.controlpanel.js
@@ -1,10 +1,55 @@
-$(function() {
-    $( "#panel1" ).draggable();
-    $( "#panel1" ).resizable();
+if(window.EEA === undefined){
+  var EEA = {
+    who: 'eea.controlpanel',
+    version: '1.0'
+  };
+}
 
-    $( "#panel2" ).draggable();
-    $( "#panel2" ).resizable();
+EEA.ControlPanelInit = function(){
+    var panels = $("div[id^='panel']");
+    jQuery.each(panels, function(index, element){
+        element = $(element);
+        element.draggable();
+        element.resizable();
+    });
+};
 
-    $( "#panel3" ).draggable();
-    $( "#panel3" ).resizable();
+EEA.ControlPanelDbActivity = function(){
+    var panel = $('#panel3');
+    panel.children('.spinner').hide();
+    jQuery.ajax({
+        url: '@@eea.controlpaneldb.html',
+        data: {},
+        success: function(data, textStatus, jqXHR){
+            var logs = data['log'];
+            jQuery.each(logs, function(index, element){
+                var element_html = $('<div></div>');
+                var username = jQuery.trim(element['user_name'].substring(4, element['user_name'].length));
+                element_html.append(element['description'] + '<br />');
+                element_html.append(element['time'] + '  |  ');
+                element_html.append('<a title="" href="/author/' + username+ '">' + username + '</a>' + '  |  ');
+                element_html.append(element['size'] + ' bytes');
+                panel.children('.container').append(element_html);
+            });
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            console.log(errorThrown);
+        }
+    });
+};
+
+jQuery(document).ready(function(){
+
+    // Setup panels
+    EEA.ControlPanelInit();
+
+    // Populate database activity panel
+    EEA.ControlPanelDbActivity();
+
+    // Populate today login panel
+    // TODO
+
+    // Populate login history panel
+    // TODO
+
 });
