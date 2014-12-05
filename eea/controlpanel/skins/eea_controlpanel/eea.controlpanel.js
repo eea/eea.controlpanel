fdc60dd9d3e989d4d8da3dd6787c236516db7a9b
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2014-12-05 18:16:03 +0200

    Implemented auto update of the panels
    Implemented the auto-update of the panels and the manual triggered
    refresh.
    Fixed/updated CSS of the panels.
    refs #21853

--- eea/controlpanel/skins/eea_controlpanel/eea.controlpanel.js
+++ eea/controlpanel/skins/eea_controlpanel/eea.controlpanel.js
@@ -12,14 +12,32 @@ EEA.ControlPanelInit = function(){
         element.draggable();
         element.resizable();
     });
+
+    $( "#panel1 h3 span .eea-icon-refresh" ).click(function() {
+        EEA.ControlPanelLoginStatus();
+    });
+
+    $( "#panel3 h3 span .eea-icon-refresh" ).click(function() {
+        EEA.ControlPanelDbActivity();
+    });
+
+    // Refresh "Login status" panel
+    var login_status = new EEA.ControlPanelLoginStatus();
+    login_status.refresh();
+
+    // Refresh "DB activity" panel
+    var login_status = new EEA.ControlPanelDbActivity();
+    login_status.refresh();
 };
 
 EEA.ControlPanelDbActivity = function(){
     var panel = $('#panel3');
+    $('#panel3 .container').css({ "background-color": "rgb(221, 221, 221)" });
     jQuery.ajax({
         url: '@@eea.controlpaneldb.html',
         data: {},
         success: function(data, textStatus, jqXHR){
+            $('#panel3 .container').html('');
             var logs = data['log'];
             jQuery.each(logs, function(index, element){
                 var element_html = $('<div></div>');
@@ -29,6 +47,7 @@ EEA.ControlPanelDbActivity = function(){
                 element_html.append('<a title="" href="/author/' + username+ '">' + username + '</a>' + '  |  ');
                 element_html.append(element['size'] + ' bytes');
                 panel.children('.container').append(element_html);
+                $('#panel3 .container').css({ "background-color": "#f0f0f0" });
             });
         },
         error: function(jqXHR, textStatus, errorThrown){
@@ -36,6 +55,14 @@ EEA.ControlPanelDbActivity = function(){
         }
     });
 };
+
+EEA.ControlPanelDbActivity.prototype = {
+    refresh: function(){
+    setInterval(function () {
+        EEA.ControlPanelDbActivity();
+    }, 60000);
+    }
+}
 
 EEA.ControlPanelLoginStatusAgent = function(){
     jQuery.ajax({
@@ -56,10 +83,12 @@ EEA.ControlPanelLoginStatusAgent = function(){
 
 EEA.ControlPanelLoginStatus = function(){
     var panel = $('#panel1');
+    $('#panel1 .container').css({ "background-color": "rgb(221, 221, 221)" });
     jQuery.ajax({
         url: '@@eea.controlpanelloginstatus.html',
         data: {},
         success: function(data, textStatus, jqXHR){
+            $('#panel1 .container').html('');
             var logs = data['active_users'];
             jQuery.each(logs, function(index, element){
                 var element_html = $('<div></div>');
@@ -69,6 +98,7 @@ EEA.ControlPanelLoginStatus = function(){
                 element_html.append('<a title="" href="/author/' + index + '">' + element[0] + '</a>');
                 element_html.append(' :: ' + element[2]);
                 panel.children('.container').append(element_html);
+                $('#panel1 .container').css({ "background-color": "#f0f0f0" });
             });
         },
         error: function(jqXHR, textStatus, errorThrown){
@@ -76,6 +106,14 @@ EEA.ControlPanelLoginStatus = function(){
         }
     });
 };
+
+EEA.ControlPanelLoginStatus.prototype = {
+    refresh: function(){
+    setInterval(function () {
+        EEA.ControlPanelLoginStatus();
+    }, 300000);
+    }
+}
 
 jQuery(document).ready(function(){
     // Setup panels
